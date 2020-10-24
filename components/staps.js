040928cbeb45574def808ec2652a41c1a847846e
Benqqa <53792741+Benqqa@users.noreply.github.com>
2020-10-24 18:07:41 +0300

    Create staps.js

---
+++ components/staps.js
@@ -0,0 +1,13 @@
+Vue.component('staps', {
+    template: `
+    			<div class="staps">
+    				<ul>
+    					<li class="staps__stap stap1"></li>
+    					<li class="staps__stap stap2"></li>
+    					<li class="staps__stap stap3"></li>
+    					<li class="staps__stap stap4"></li>
+    					<li class="staps__stap stap5"></li>
+    				</ul>
+    			</div>
+    		`
+})
