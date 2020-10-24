07fa1a4c621e9ed6c1f1e1df8b4df76e6f01acbc
Benqqa <53792741+Benqqa@users.noreply.github.com>
2020-10-24 16:23:03 +0300

    Update page1.js

--- components/page1.js
+++ components/page1.js
@@ -1,18 +1,19 @@
 Vue.component('page1', {
     template: `
     				<div class="page1">
-    					asdad
+    						<div class="page1__text"><p>Начни инвестировать<p> <p> \n прямо сейчас!<p></div>
+								<div class="page1__img"><img src="img/finance_analytics_monochromatic.svg" alt=""></div>
     					<div class="waveWrapper waveAnimation">
-  <div class="waveWrapperInner bgTop">
-    <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
-  </div>
-  <div class="waveWrapperInner bgMiddle">
-    <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
-  </div>
-  <div class="waveWrapperInner bgBottom">
-    <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
-  </div>
-</div>
+						  <div class="waveWrapperInner bgTop">
+						    <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
+						  </div>
+						  <div class="waveWrapperInner bgMiddle">
+						    <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
+						  </div>
+						  <div class="waveWrapperInner bgBottom">
+						    <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
+						  </div>
+						</div>
     				</div>
     		`
 })
