41e9212f33feffabb68ebb8a751ecd5b66a17e19
ElvenVillage <37876353+ElvenVillage@users.noreply.github.com>
2020-10-25 02:55:45 +0300

    смерть

--- components/secondSlider/index.js
+++ components/secondSlider/index.js
@@ -1,7 +1,9 @@
 Vue.component('second-slider', {
     template: `
     <swiper :options="secondSwiperOptions">
-     
+     <div class="swiper-pagination" slot="pagination"></div>
+     <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
     </swiper>
     `,
     data: function () {
