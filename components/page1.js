c92a2bc797e13784c2a4e646d47df902e97f55ad
Benqqa <53792741+Benqqa@users.noreply.github.com>
2020-10-25 02:21:18 +0300

    Update page1.js

--- components/page1.js
+++ components/page1.js
@@ -3,10 +3,12 @@ Vue.component('page1', {
     				<div class="page1">
     					<header1></header1>
     						<div class="page1__text"><p>Начни инвестировать<p> <p> \n прямо сейчас!</p></div>
+    						<a href="index.html" class="page1__btn">Далее</a>
 								<div class="page1__img"><img src="img/finance_analytics_monochromatic.svg" alt=""></div>
+
     					<div class="waveWrapper waveAnimation">
 						  <div class="waveWrapperInner bgTop">
-						    <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
+						    <div class="wave waveTop" style="background-image: url('img/wave-top.png')"></div>
 						  </div>
 						  <div class="waveWrapperInner bgMiddle">
 						    <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
