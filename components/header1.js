9aa2b6ae28ecc6c1da450870ec476b77f148512c
Benqqa <53792741+Benqqa@users.noreply.github.com>
2020-10-24 16:34:44 +0300

    Update header1.js

--- components/header1.js
+++ components/header1.js
@@ -16,37 +16,13 @@ Vue.component('header1', {
 						</div>
 						<nav class="menu__body">
 							<ul class="menu__list">
-								<li><a href="#adress" class="menu__link">Адреса</a></li>
-								<li><a href="#" class="menu__link">Партнерам</a></li>
-								<li><a href="#sale" class="menu__link">Акции</a></li>
-								<li><a href="#" class="menu__link">Компаниям</a></li>
-								
+								<li><a href="#adress" class="menu__link">Открыть Брокерский счет</a></li>
+								<li><a href="#" class="menu__link">Научится</a></li>
+								<li><a href="#sale" class="menu__link">О компании</a></li>
 							</ul>
 						</nav>
 					</div>
 					<div class="nav__actions act-nav">
-						<a class="act-nav__region"><span>+79819030068</span></a>
-						<div class="act-nav__user user-nav">
-							<div class="user-nav__icon"><img src="icon/ico_man_2.svg" alt=""></div>
-							<ul class="user-nav__menu">
-								
-					
-										<li>
-											<a href="" class="user-nav__link user_nav__link1">vk</a>
-										</li>
-										<li>
-											<a href="" class="user-nav__link user_nav__link2">whatsapp</a>
-										</li>
-										<li>
-											<a href="" class="user-nav__link user_nav__link3">instagram</a>
-										</li>
-										<li>
-											<a href="" class="user-nav__link user_nav__link4">telegram</a>
-										</li>
-								
-							</ul>
-							
-						</div>
 						
 					</div>
 			</div>
