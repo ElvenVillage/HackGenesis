382bc4699c0611e137a53fff0567037b7bdc7d15
Benqqa <53792741+Benqqa@users.noreply.github.com>
2020-10-25 00:42:57 +0300

    Create hard_work.js

---
+++ components/hard_work.js
@@ -0,0 +1,41 @@
+Vue.component('hard_work', {
+    template: `		
+    <div class="hard_work">
+    		<div class="hard_work__work"><img src="img/nadp.png" alt=""></div>
+    		<div class="hard_work__smarter"><img src="img/smarter.webp" alt=""></div>
+    		<div class="hard_work__question">
+    			<div class="hard_work__task">
+    				Почему стоит выбрать "БКС Брокер"?
+    			</div>
+    		</div>
+    		<div class="container">
+    		<div class="hard_work__Htext">
+    			<p>Мы - крупнейшая брокерская фирма</p>
+    			<p>с отличными показателями:</p>
+    		</div>
+    		<div class="hard_work__infos">
+    			<div class="hard_work__info">
+    				<div class="hard_work__value">260</div>
+    				<div class="hard_work__text">млрд рублей активов</div>
+    			</div>
+    			<div class="hard_work__info">
+    				<div class="hard_work__value">25</div>
+    				<div class="hard_work__text">лет на рынке</div>
+    			</div>
+    			<div class="hard_work__info">
+    				<div class="hard_work__value">1</div>
+    				<div class="hard_work__text">место в рейтинге московской биржи</div>
+    			</div>
+    			<div class="hard_work__info">
+    				<div class="hard_work__value">ААА</div>
+    				<div class="hard_work__text">рейтинг надежности</div>
+    			</div>
+    			<div class="hard_work__info">
+    				<div class="hard_work__value">600+</div>
+    				<div class="hard_work__text">тысяч клиентов</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    `
+})
