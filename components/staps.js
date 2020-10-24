b3dd92947015af1e4f74f4c4d77694666f202fbb
ElvenVillage <37876353+ElvenVillage@users.noreply.github.com>
2020-10-25 00:28:45 +0300

    fullpage

--- components/staps.js
+++ components/staps.js
@@ -1,18 +1,16 @@
 Vue.component('staps', {
     template: `
-    			<div class="staps">
-    				<div class="container">
-    <div class="page-header">
-    </div>
-    <ul class="timeline">
-        <li class="timeline-inverted">
-          <div class="timeline-badge warning">1<i class="glyphicon glyphicon-credit-card"></i></div>
-          <div class="timeline-panel">
-          		<first-slider></first-slider>
-          </div>
-        </li>
-    </ul>
-</div>
-    			</div>
+    <div class="staps">
+        <div class="container">
+            <div class="page-header">
+            </div>
+        <ul class="timeline">
+            <li class="timeline-inverted">
+              <div class="timeline-badge warning">1<i class="glyphicon glyphicon-credit-card"></i></div>
+              <div class="timeline-panel"></div>
+            </li>
+        </ul>
+        </div>
+      </div>
     		`
 })
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 Vue.use(VueAwesomeSwiper)
+Vue.use(VueFullpage)
 
 MASTERKEY = 0
 
 new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        opts: {
+            start: 0,
+        }
+    }
 })
