5f2d5badfdbd1eafcf902012e3d3a77934744b54
ElvenVillage <37876353+ElvenVillage@users.noreply.github.com>
2020-10-25 04:45:56 +0300

    step2

--- components/step1.js
+++ components/step1.js
@@ -1,3 +1,5 @@
+const isMobile = window.screen.availWidth <= 425
+
 Vue.component('step-2', {
     template: `
     <div class="staps" style="font-family: 'arial_bold', Arial, sans-serif">
@@ -5,7 +7,7 @@ Vue.component('step-2', {
             <div class="page-header">
             </div>
             <div style="display: flex; flex-direction: row"><div style="flex-grow: 1; flex-direction: column">
-        <ul class="timeline">
+        <ul class="timeline" v-if="!isMobile">
             <li class="timeline-inverted">
               <div class="timeline-badge warning">2<i class="glyphicon glyphicon-credit-card"></i></div>
               <div class="timeline-panel" style="height: 100vh"></div>
@@ -13,15 +15,15 @@ Vue.component('step-2', {
         </ul>
         
         </div><div style="flex-grow: 3">
-        <div style="font-weight: bold; font-size: large">Шаг 2. Получите бесплатные рекомендации по подбору пакетов</div>
+        <div style="font-weight: bold; font-size: large; margin: 5px">Шаг 2. Получите бесплатные рекомендации</div>
         <div style="flex-direction: column">
         <table v-for="(pr, idx) in investProducts[MASTERKEY]" :key="idx"
                style="background: linear-gradient(to right, #421b70, #4f76ac);
-               width: 600px; height: 100px; color: white; margin: 50px; text-align: left">
-        <tr><td><img :src="pr.imgsrc" style="width: 200px; height: 100px; padding: 20px"></td><td>
+               width: 600px; height: 100px; color: white; margin: ${(isMobile)? '-10px' : '50px'}; text-align: left">
+        <tr><td style="width: ${(isMobile)? window.screen.availWidth/4 : '150px'}"><img :src="pr.imgsrc" style="width: 150px; height: 150px; padding: 20px"></td><td>
         <table><tr><td>{{ pr.type }}</td></tr>
                <tr><td>{{ pr.title }}</td></tr>
-               <tr><td>{{ pr.percent }}</td><td>{{ pr.price }}</td></tr></table>
+               <tr><td style="width: 200px;">{{ pr.percent }}</td><td>{{ pr.price }}</td></tr></table>
          </td></tr>
         </table>
         </div></div>
@@ -31,21 +33,22 @@ Vue.component('step-2', {
     data: function () {
         return {
             expanded: [true, false, false],
+            isMobile: isMobile,
             investProducts: [[
                 {
-                    imgsrc: 'https://pngimg.com/uploads/yandex/yandex_PNG23.png',
+                    imgsrc: 'img/yandex-icon.png',
                     title: 'Яндекс',
                     percent: 'до 14,5%',
                     price: 'от 1500 рублей',
                     type: '#Акции'
                 }, {
-                    imgsrc: 'https://free-png.ru/wp-content/uploads/2020/09/sberbank__2-01.png',
+                    imgsrc: 'img/sber.png',
                     title: 'Сбербанк',
                     percent: 'до 6%',
                     price: 'от 10000 рублей',
                     type: '#Защищенные инвестиции'
                 }, {
-                    imgsrc: 'https://pngimg.com/uploads/amazon/amazon_PNG28.png',
+                    imgsrc: 'img/amazon.png',
                     title: 'Amazon',
                     percent: 'до 6%',
                     price: 'от 10000 рублей',
